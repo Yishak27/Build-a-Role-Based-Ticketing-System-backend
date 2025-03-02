f12d9aee3c9dd0478654ac310260b181c5306cd7
PC <PC@DESKTOP-F726TKR>
2025-03-02 21:45:02 +0300

    user modal is added

---
+++ model/UserSchema.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const SettingsSchema = require("./SettingsSchema");
+const userSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        require: false,
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        require: false,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+        require: false,
+    },
+    isLocked: {
+        type: Boolean,
+        default: false,
+        require: false,
+    },
+    lastLogin: {
+        type: Date,
+        require: false
+    },
+    token: {
+        type: String,
+        require: false
+    },
+    roleCode: [{
+        type: String,
+        require: false,
+        enum: ["user", "admin", "superadmin"]
+    }],
+    status: {
+        type: String,
+        enum: ["Active", "Pending", "Inactive"],
+        default: "Active"
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
